8759b8fbbc71a6ab6822d4e5c9f04ac550732f35
Prakash Shankar <prakashankar39@gmail.com>
2022-03-17 15:34:22 -0400

    login/signup added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,49 @@
 // Login Page. Prakash will enter his login code here 
+
+/*
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TeamUp - Login</title>
+    <link rel="stylesheet" href="css/style.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <!-- login form starts -->
+    <div class="wrapper login" id="wrapper">
+        <div class="container">
+            <div class="tabs">
+                <ul>
+                  <li class="sign_in_li">Sign in</li>
+                </ul>
+            </div>
+            <div class="input_field">
+                <input type="text" placeholder="Username" class="input">
+            </div>
+            <div class="input_field">
+                <input type="password" placeholder="Password" class="input">
+            </div>
+            <div class="btn"><a href="#">Sign in</a></div>
+            <li><a href="register.html" class="next-page">Not a user?  Sign Up</a></li> 
+        </div>
+    </div>
+    <!-- login form ends -->
+
+</body>
+<script src="https://code.jquery.com/jquery-3.6.0.js""></script>
+
+</html>
+
+*/
+
+
+
 export default function Login() {
     return (
         <section class="home" id="home">
