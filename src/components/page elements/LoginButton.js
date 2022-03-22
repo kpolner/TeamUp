143008c331c6be170588b9f861c065614039394d
kpolner <kylepolner@gmail.com>
2022-03-22 17:45:12 -0400

    login filter and match

--- src/components/page elements/LoginButton.js
+++ src/components/page elements/LoginButton.js
@@ -5,7 +5,12 @@ import GlobalLogin from "./GlobalLogin";
 export class LoginButton extends React.Component {
     constructor(props) {
         super(props);
-    };
+        this.state = {
+            username1: "",
+            password: "",
+            userlist: []
+        }
+        };
 
     async login() {
         console.log(this.props.name)
@@ -18,6 +23,29 @@ export class LoginButton extends React.Component {
                 }
             }
         )
+        let currentUser = response.data.results;
+        this.setState({userlist: currentUser});
+
+        let inputtedUser = document.getElementById("name").value;
+        let inputtedPassword = document.getElementById("loginpassword").value;
+
+        let match = userlist.filter(currentUser => currentUser.username = inputtedUser);
+        if (match.length === 0)
+        {
+            alert("Incorrect username or password.");
+        }
+        else if (match[0].password != inputtedPassword)
+        {
+            alert("Incorrect username or password.");
+        }
+        else if (match[0].password === inputtedPassword)
+        {
+            // login the user
+            // provide functionality to events
+            // set the home page display to the user
+            // change the navbar to user, 'sign out', 'discover'
+        }
+
         // store jwt here local storage
         // use it to validate endpoint you use through out application
     };
